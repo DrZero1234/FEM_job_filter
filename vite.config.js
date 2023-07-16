971b2cc11a7f864ed78a95a0c634464d2d7a7903
M.Kristóf <drzero123456@gmail.com>
2023-07-16 12:21:10 +0200

    path fixed

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   const config = {
     plugins: [react()],
     base: '/',
   }
 
-  if (command !== 'serve') {
-    config.base = '/FEM_JOB_FILTER/'
-  }
+  config.base = '/FEM_job_filter/'
 
   return config
 })
